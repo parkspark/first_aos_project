82a3084ae683f224e4d9e2545e94698c6f58742a
parkspark <rkatk1760@gmail.com>
2023-05-04 12:26:16 +0900

    한글 폰트 추가

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.resources import resource_add_path
+
+resource_add_path('.')
+LabelBase.register(DEFAULT_FONT, "./MaruBuriTTF/MaruBuri-Regular.ttf")
 
 class KivyMemoApp(App):
     def build(self):
